0888aedd07dea76352424cac62e0d5d2c7128ec3
smrssll <102466515+smrssll@users.noreply.github.com>
2022-07-01 02:08:58 +1200

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -65,7 +65,7 @@ export const Banner = () => {
                 >
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    {`Hi, I'm Sam,'}{" "}
+                    {`Hi! I'm Sam,`}{" "}
                     <span
                       className="txt-rotate"
                       dataPeriod="1000"
